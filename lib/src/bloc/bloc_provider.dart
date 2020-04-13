676186d476b7c6530dfb2db9a2d1fca80aa8c2c1
Diego Lascano <diegolascanodalgo@gmail.com>
2020-04-13 15:30:01 -0500

    inherited widget name changed due to duplicated file in other folder

--- lib/src/bloc/bloc_provider.dart
+++ lib/src/bloc/bloc_provider.dart
@@ -2,16 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:score_mama/src/bloc/score_mama_bloc.dart';
 export 'package:score_mama/src/bloc/score_mama_bloc.dart';
 
-class DataProvider extends InheritedWidget {
+class BlocProvider extends InheritedWidget {
   final loginBloc = ScoreMamaBloc();
 
-  DataProvider({Key key, Widget child}) : super(key: key, child: child);
+  BlocProvider({Key key, Widget child}) : super(key: key, child: child);
 
   @override
   bool updateShouldNotify(InheritedWidget oldWidget) => true;
 
   static ScoreMamaBloc of(BuildContext context) {
-    return (context.dependOnInheritedWidgetOfExactType<DataProvider>())
+    return (context.dependOnInheritedWidgetOfExactType<BlocProvider>())
         .loginBloc;
   }
 }
